173bd9ff8032ddfdc126df9f9ba5b59baaa2e2c0
Anil Khadka <anil.khadka3777@gmail.com>
2020-02-23 14:44:32 +0545

    tweaked flac_to_wav.sh to delete converted .flac files

--- flac_to_wav.sh
+++ flac_to_wav.sh
@@ -5,4 +5,7 @@ find . -iname "*.flac" | wc
 for flacfile in `find . -iname "*.flac"`
 do
     avconv -y -f flac -i $flacfile -ab 64k -ac 1 -ar 16000 -f wav "${flacfile%.*}.wav"
+
+    #Deleting flacfile which is converted to .wav file
+    rm -rf "$flacfile"
 done
